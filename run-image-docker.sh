338881a6152b683a5c5d96010fc0f1278fc92d49
root <root@cpd1-gli-jenker.corpnet.inside>
2015-02-03 08:40:39 +0100

    run fix

--- run-image-docker.sh
+++ run-image-docker.sh
@@ -1,1 +1,12 @@
-docker run -itd -p 8000:8080 jettyapp
+#! /bin/bash
+CONTAINER_NAME=jettyapp_container
+IMAGE_NAME=jettyapp
+
+if [ -n 'docker ps | grep $CONTAINER_NAME' ]
+then
+  echo "stopping and removing container $CONTAINER_NAME"
+  docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME	
+fi
+
+docker run -itd -p 8000:8080 --name="$CONTAINER_NAME" $IMAGE_NAME
